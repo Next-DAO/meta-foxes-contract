4bb3e47cd2efd9a95c689508109cfef7ec2ffefb
shep.eth <shep@nextdao.xyz>
2023-08-02 20:11:12 +1200

    generate new metadata

--- scripts/generate_metadata.js
+++ scripts/generate_metadata.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
-const ipfsHash = "QmYFEQtoBx26TyHWZ2Y8iLRAd4DtVgKz1rKvfLQ5UgHWuJ";
+const ipfsHash = "QmWXvxLVaW1KJv7iwY3Z3EwyGJJCDSpo36NMobr3i3Qxht";
 const indexes = [...Array(500).keys()];
-const revealedUntil = 104;
+const revealedUntil = 105;
 const genesisUtil = 90;
 const metadataPath = "./metadata/";
 const femaleIds = [
