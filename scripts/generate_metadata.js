0875d9a89b375defbeb0fa24556bf28ca9529532
shep.eth <shep@nextdao.xyz>
2023-10-11 01:34:50 +1300

    update for new wallet

--- scripts/generate_metadata.js
+++ scripts/generate_metadata.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
-const ipfsHash = "QmVoHev2BTRxDb7pJyMyDauUKfwTBMiL2tojwxt8nrBpvf";
+const ipfsHash = "QmUZrPc8dYyWSAq4vkPdm2aQDYSXPeBZbqw3mNFMasDnAd";
 const indexes = [...Array(500).keys()];
-const revealedUntil = 121;
+const revealedUntil = 125;
 const genesisUtil = 90;
 const metadataPath = "./metadata/";
 const femaleIds = [
