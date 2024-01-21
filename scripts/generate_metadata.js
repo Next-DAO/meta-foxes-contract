8b3be62162ceb90d4a3cc89470d2139703679caa
shep.eth <shep@nextdao.xyz>
2024-01-22 02:14:09 +1300

    update metadata

--- scripts/generate_metadata.js
+++ scripts/generate_metadata.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const ipfsHash = "QmWBn2kWu187gtircxsZQ1MxtbYo5YmkyfeVn9pHtGmbPR";
+const ipfsHash = "QmWUynKtSQp6g7iJPQ3RyMUe3GsfUasqQsaNgyD669X51r";
 const indexes = [...Array(500).keys()];
 const revealedUntil = 153;
 const genesisUtil = 90;
@@ -28,14 +28,14 @@ const main = async () => {
       : `Meta Fox #${id}`;
 
     const image =
-      index < revealedUntil || id === 164 || id === 155 || id === 156 || id === 162 || id === 165 || id === 174 || id === 167 || id === 172
+      index < revealedUntil || id === 164 || id === 155 || id === 156 || id === 162 || id === 165 || id === 174 || id === 167 || id === 172 || id === 173
         ? `ipfs://${ipfsHash}/${id}.png`
         : `ipfs://${ipfsHash}/unrevealed.png`;
 
     const generation = index < genesisUtil ? "Genesis" : "Gen II";
     const attributes = [{ trait_type: "Generation", value: generation }];
 
-    if (index < revealedUntil || id === 164 || id === 155 || id === 156 || id === 162 || id === 165 || id === 174 || id === 167 || id === 172) {
+    if (index < revealedUntil || id === 164 || id === 155 || id === 156 || id === 162 || id === 165 || id === 174 || id === 167 || id === 172 || id === 173) {
       attributes.push({ trait_type: "Gender", value: gender });
     }
 
