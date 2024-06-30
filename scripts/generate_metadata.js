a0c0d81ea627b39f65d65553341b5e760ad515d0
shep.eth <shep@nextdao.xyz>
2024-07-01 02:20:36 +1200

    add more images

--- scripts/generate_metadata.js
+++ scripts/generate_metadata.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const ipfsHash = "QmdqPdD647kRV5cuSde8hdVwrHFHyjCv2dgJnH7vVxrtYa";
+const ipfsHash = "QmcA5ahT2bCTNCCXVPgsxhTv8zcHXBfpQfikWxWCadd2eY";
 const indexes = [...Array(200).keys()];
 const revealedUntil = 190;
 const genesisUtil = 90;
@@ -8,7 +8,7 @@ const metadataPath = "./metadata/";
 const femaleIds = [
   12, 10, 17, 28, 31, 34, 36, 44, 45, 46, 47, 51, 55, 57, 59, 65, 73, 74, 76,
   81, 82, 83, 86, 87, 88, 89, 92, 97, 104, 110, 113, 119, 121, 129, 130, 132,
-  137, 138, 144, 155, 167, 168, 172, 184, 190, 196,
+  137, 138, 144, 155, 167, 168, 172, 184, 190, 195, 196,
 ];
 const desc =
   "Meta Foxes is an NFT collection initiated by the NextDAO community. Every Meta Fox is painstakingly crafted and customized by artists for its owner, without using any artificial intelligence or automation technology. We are committed to using unique designs for each NFT, highlighting its unique story and personality, making it a valuable collectible.";
@@ -23,6 +23,8 @@ const main = async () => {
     const urgent =
       id === 192 ||
       id === 193 ||
+      id === 194 ||
+      id === 195 ||
       id === 196 ||
       id === 199 ||
       id === 200;
