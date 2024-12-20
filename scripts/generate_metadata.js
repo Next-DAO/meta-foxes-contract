675633ed4374831ed8349cce5f00ce3222124973
shep.eth <shep@nextdao.xyz>
2024-12-20 16:06:59 +1300

    update metadata

--- scripts/generate_metadata.js
+++ scripts/generate_metadata.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 
-const ipfsHash = "QmQoBuff8xQNbNtF6WJfzag17CAZonnw5Rt4roykJMPKnF";
-const indexes = [...Array(200).keys()];
-const revealedUntil = 200;
+const ipfsHash = "bafybeidixflepaqg5dukpikjkrrf3c7kp5p5fkz35verwy6lppzrf7vnhe";
+const indexes = [...Array(222).keys()];
+const revealedUntil = 201;
 const genesisUtil = 90;
 const metadataPath = "./metadata/";
 const femaleIds = [
   12, 10, 17, 28, 31, 34, 36, 44, 45, 46, 47, 51, 55, 57, 59, 65, 73, 74, 76,
   81, 82, 83, 86, 87, 88, 89, 92, 97, 104, 110, 113, 119, 121, 129, 130, 132,
-  137, 138, 144, 155, 167, 168, 172, 184, 190, 195, 196,
+  137, 138, 144, 155, 167, 168, 172, 174, 184, 190, 195, 196,
 ];
 const desc =
   "Meta Foxes is an NFT collection initiated by the NextDAO community. Every Meta Fox is painstakingly crafted and customized by artists for its owner, without using any artificial intelligence or automation technology. We are committed to using unique designs for each NFT, highlighting its unique story and personality, making it a valuable collectible.";
 
-const stolenIds = [18];
+const stolenIds = [18, 21];
 const stolenDesc =
-  "DO NOT BUY! THIS TOKEN WAS STOLEN IN A PHISHING ATTACK! IMAGE ALTERED BASED ON https://snapshot.org/#/nextdao.eth/proposal/0xc6aebb10a8036fef00a59305c87f83bcb78d168d26ceec82105f90c2c9b20a5a";
+  "DO NOT BUY! THIS TOKEN WAS STOLEN IN A PHISHING ATTACK! IMAGE ALTERED BASED ON VOTES ON https://snapshot.box/#/s:nextdao.eth";
 
 const main = async () => {
   for (const index of indexes) {
