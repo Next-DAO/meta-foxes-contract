4748f4b271df60f7435a84409659face43d478b4
shep.eth <shep@nextdao.xyz>
2025-01-03 00:23:11 +1300

    add metadta for 202

--- scripts/generate_metadata.js
+++ scripts/generate_metadata.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const ipfsHash = "bafybeihg2jrosi5xkzdsosbze6byswmo3lkw5xlcytqv6aeu5mbivp62qm";
+const ipfsHash = "bafybeicgwmdice5io3kzanebbqbwk2gzpk775cdyj5umh3wikui6tv4lku";
 const indexes = [...Array(222).keys()];
 const revealedUntil = 201;
 const genesisUtil = 90;
@@ -8,7 +8,7 @@ const metadataPath = "./metadata/";
 const femaleIds = [
   12, 10, 17, 28, 31, 34, 36, 44, 45, 46, 47, 51, 55, 57, 59, 65, 73, 74, 76,
   81, 82, 83, 86, 87, 88, 89, 92, 97, 104, 110, 113, 119, 121, 129, 130, 132,
-  137, 138, 144, 155, 167, 168, 172, 174, 184, 190, 195, 196,
+  137, 138, 144, 155, 167, 168, 172, 174, 184, 190, 195, 196, 202
 ];
 const desc =
   "Meta Foxes is an NFT collection initiated by the NextDAO community. Every Meta Fox is painstakingly crafted and customized by artists for its owner, without using any artificial intelligence or automation technology. We are committed to using unique designs for each NFT, highlighting its unique story and personality, making it a valuable collectible.";
@@ -21,7 +21,7 @@ const main = async () => {
   for (const index of indexes) {
     const id = index + 1;
 
-    const urgent = id == 203;
+    const urgent = id === 202 || id === 203;
 
     const gender = femaleIds.includes(id) ? "Female" : "Male";
 
